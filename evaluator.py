6b6849256d2e47628a3d6a6d544a684919a38d16
jsantos805 <jacob.santos093@gmail.com>
2026-08-27 12:10:42 +0930

    added whitespace and number branch in the code

--- evaluator.py
+++ evaluator.py
@@ -5,3 +5,11 @@ def tokenize(text):
     i=0
     while i < len(text):
         char=text[i]
+if char.isspace():
+    i==1+ 
+    continue
+
+if char.isdigit():
+    start=i
+    while i < len(text) and text[i].isdigit():
+        
