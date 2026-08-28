58d59735aacca25751f921177e842cf7e3f846a9
MarkoRegojevic <S406564@students.cdu.edu.au>
2026-08-28 21:22:11 +0930

    I have added a new block which will dead with multiplication, division, and remainder. And also implicit multiplication.

--- evaluator.py
+++ evaluator.py
@@ -17,8 +17,8 @@ if char.isdigit():
     if i < len(text) and text[i] == '.':
         i+= 1
         if i>= len(text) or not text[i].isdigit():
-            raie ValueError (f"bad number format at position {start}")
-        while i< len(text) or not text[i].isdigit():
+            raise ValueError (f"bad number format at position {start}")
+        while i< len(text) and text[i].isdigit():
             i+= 1
         tokens.append(('NUMBER', float(text[start:i])))
         continue
@@ -33,6 +33,23 @@ if char.isdigit():
         left = (operator, left, right)
     return left, position 
 
+# This block will deal with multuplication, division and remainder, and also implicit multiplication. . 
+def term(tokens, position): 
+    left, position = unary(tokens, position) 
 
+while true: 
+    if tokens[position][0] == "OP" and tokens[position][1] in ("*", "/", "%"):
+        operator = tokens[position][1]
+        right, position = unary(tokens, position + 1)
+        left = (operator, left, right)
+
+    elif tokens[position][0] == "NUM" :
+        raise ValueError()
     
-     
+elif tokens[position][0] == "LPAREN":
+right, position = unary(tokens, position)
+left = ("*", left, right) 
+else: 
+    break
+return left, position 
+
