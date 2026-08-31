c565d7d704aee8ffc81b4f205ec625d8854e3fd7
jsantos805 <jacob.santos093@gmail.com>
2026-08-31 23:41:03 +0930

    adjusted spaces and indentations

--- evaluator.py
+++ evaluator.py
@@ -2,25 +2,34 @@ import os
 
 # makes the tokens from expression
 def tokenize(text):
+
     i=0
     while i < len(text):
+
         char=text[i]
+
 if char.isspace():
     i +=1 
     continue
 
 if char.isdigit():
+
     start=i
+
     while i < len(text) and text[i].isdigit():
        i+= 1
+
     if i < len(text) and text[i] == '.':
         i+= 1
+
         if i>= len(text) or not text[i].isdigit():
             raise ValueError (f"bad number format at position {start}")
+
         while i< len(text) or not text[i].isdigit():
             i+= 1
-        tokens.append(('NUMBER', float(text[start:i])))
-        continue
+
+    tokens.append(('NUMBER', float(text[start:i])))
+    continue
 
  #   This block will deal wit the plus and minus
  def expression(tokens, position): 
